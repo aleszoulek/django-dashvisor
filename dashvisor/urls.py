ce9431a084d33346b59a319fd7ec39eb6e887935
Ales Zoulek <ales.zoulek@gmail.com>
2011-09-12 20:22:20 -0700

    unused import

--- dashvisor/urls.py
+++ dashvisor/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('dashvisor.views',
     url(r'^$', 'dashboard', name='dashboard'),
