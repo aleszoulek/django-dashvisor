960558675f3aeae4a73c13681af4d23d94b51a35
Ales Zoulek <ales.zoulek@gmail.com>
2011-09-20 01:33:35 -0700

    xmlrpc status + some html

--- dashvisor/server.py
+++ dashvisor/server.py
@@ -1,3 +1,13 @@
+from xmlrpclib import ServerProxy
+from urlparse import urlparse
+
+
+
 class Server(object):
     def __init__(self, connection_string):
-        self.connection_string = connection_string
+        self.name = urlparse(connection_string).hostname
+        self.connection = ServerProxy(connection_string)
+        self.refresh()
+
+    def refresh(self):
+        self.status = self.connection.supervisor.getAllProcessInfo()
