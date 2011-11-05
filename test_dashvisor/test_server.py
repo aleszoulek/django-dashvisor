ec761226a1be0acde7edf46180cab9ce386634e1
Ales Zoulek <ales.zoulek@gmail.com>
2011-11-05 02:23:19 +0100

    test server.start and server.stop

--- test_dashvisor/test_server.py
+++ test_dashvisor/test_server.py
@@ -9,6 +9,19 @@ from test_dashvisor.cases import SupervisorTestCase
 class TestServer(SupervisorTestCase):
     def setUp(self):
         self.server = backend.servers['localhost']
+        self.server.start_all()
+
+    def _assert_stopped(self, name):
+        self._assert_state(name, 0)
+
+    def _assert_running(self, name):
+        self._assert_state(name, 20)
+
+    def _assert_state(self, name, state):
+        tools.assert_equals(
+            self.server.status['a:x']['state'],
+            state
+        )
 
     def test_refresh(self):
         tools.assert_equals(
@@ -20,5 +33,21 @@ class TestServer(SupervisorTestCase):
             self.server.status.keys(),
             ['a:x', 'a:y', 'b:y', 'b:z', 'tail-everything:tail-everything']
         )
+
+    def test_stop(self):
+        self._assert_running('a:x')
+        self.server.stop('a:x')
+        self._assert_running('a:x')
+        self.server.refresh()
+        self._assert_stopped('a:x')
+
+    def test_start(self):
+        self.server.stop('a:x')
+        self.server.refresh()
+        self._assert_stopped('a:x')
+        self.server.start('a:x')
+        self._assert_stopped('a:x')
+        self.server.refresh()
+        self._assert_running('a:x')
 
 
